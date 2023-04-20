8d9558394b6afff28b8a0238be697767b63cde29
Savio Viana <savios138@gmail.com>
2023-04-20 10:59:40 -0300

    files course commit

---
+++ exercises/leituraNúmero.js
@@ -0,0 +1,40 @@
+let numero = 9
+
+switch(numero) {
+    case 0:
+        console.log('zero')
+        break
+    case 1:
+        console.log('um')
+        break
+    case 2:
+        console.log('dois')
+        break
+    case 3:
+        console.log('três')
+        break
+    case 4:
+        console.log('qautro')
+        break
+    case 5:
+        console.log('cinco')
+        break
+    case 6:
+        console.log('seis')
+        break
+    case 7:
+        console.log('sete')
+        break
+    case 8:
+        console.log('oito')
+        break
+    case 9:
+        console.log('nove')
+        break
+    case 10:
+        console.log('dez')
+        break
+    default:
+        console.log('número fora do intervalo!')
+}
+
