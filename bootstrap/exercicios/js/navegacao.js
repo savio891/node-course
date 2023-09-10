91214b0c83a7a4d6098af8a199dea476177e0400
Savio Viana <savios138@gmail.com>
2023-09-10 16:29:16 -0300

    files course commit

--- bootstrap/exercicios/js/navegacao.js
+++ bootstrap/exercicios/js/navegacao.js
@@ -12,7 +12,7 @@
             .then(resp => resp.text())
             .then(html => {
                 destino.innerHTML = html
-                const resultado = html.match(/\<script\>([\s\S]*)\<\/script\>/) //Pegar todo o texto dentro do html dentro da tag script, incluindo as quebras de linhas ([\s\S]), e interpretá-los com a função eval().
+                const resultado = html.match(/\<script\>([\s\S]*)\<\/script\>/)
                 if(resultado && resultado.length >= 2) {
                     eval(resultado[1])
                 }
